1f5e83756171fe4bf98a4e107001fd1298f94fd7
Stefandixon1997 <31378143+Stefandixon@users.noreply.github.com>
2018-03-28 10:21:13 +1300

    Add files via upload
    Practical Task 1

---
+++ PracticalTask1/Character.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+public class Character
+{ 
+        string name;
+        string type;
+        int health;
+        int weaponEffect;
+        int gameCredit;
+
+        public void setName(string newName)
+        {
+            name = newName;
+        }
+        public void setType(string newType)
+        {
+            type = newType;
+        }
+        public void setHealth(int newHealth)
+        {
+            health = newHealth;
+        }
+        public void setWeaponEffect(int newWeaponEffect)
+        {
+            weaponEffect = newWeaponEffect;
+        }
+        public void setGameCredit(int newGameCredit)
+        {
+            gameCredit = newGameCredit;
+        }
+        public string toString()
+        {
+            return ("Name: " + name + ", " + "Type: " + type + ", " + "Health: " + health + ", " + "Weapon effect: " + weaponEffect + ", " +"Game Credit: " + gameCredit);
+        }
+        public void SpendAllGameCredits()
+        {
+
+        if (type == "Team Member" & gameCredit > 0)
+        {
+            Console.WriteLine("Dean, Keizer and Todd do you want to upgrade your weapon effect? y or n");
+            string spendcreditsTME = Console.ReadLine();
+            if (spendcreditsTME == "y")
+            {
+                for (int i = 0; gameCredit > 0; i = i + 1)
+                {
+                    weaponEffect = weaponEffect + 1;
+                    gameCredit = gameCredit - 1;
+                    Console.WriteLine("Weapon Effect has been increased by 1, Weapon effect is now " + weaponEffect);
+                }
+                Console.WriteLine("Weapon Effect now equals: " + weaponEffect + " and you have  " + gameCredit + " game credits");
+            }
+            else
+            {
+                Console.WriteLine("Credits not Spent");
+            }
+        }
+
+
+        else if (type == "Civilian" & gameCredit > 0)
+        {
+            Console.WriteLine("Eddie do you want to upgrade your health?  y or n");
+            string spendcreditsC = Console.ReadLine();
+            if (spendcreditsC == "y")
+            {                                                                                           // this section of code is the calculation to spend all of the game credits
+                for (int i = 0; gameCredit > 0; i = i + 1)
+                {
+                    health = health + 5;
+                    gameCredit = gameCredit - 1;
+                    Console.WriteLine("Health has been increased by 5, health is now " + health);
+                }
+                Console.WriteLine("Health now equals: " + health + " and you have  " + gameCredit + " game credits");
+            }
+            else
+            {
+                Console.WriteLine("Credits not Spent");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Credits not Spent");
+        }
+            }
+
+        }
+        
+
+
+    
+
+
+
---
+++ PracticalTask1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PracticalTask1
+{
+    enum characters {character1, character2, character3, character4 };
+    class Program
+    {
+        
+
+        
+        static void Main(string[] args)
+        {
+            Character character1 = new Character();
+            Character character2 = new Character();
+            Character character3 = new Character();             // creating the 4 characters
+            Character character4 = new Character();
+
+            character1.setName("DeanKeaton");
+            character1.setType("Team Member");                  // Giving the objects elements
+            character1.setHealth(15);
+            character1.setWeaponEffect(4);
+            character1.setGameCredit(5);
+
+            character2.setName("Keizer Soze");
+            character2.setType("Enemy");
+            character2.setHealth(100);
+            character2.setWeaponEffect(2);
+            character2.setGameCredit(15);
+
+            character3.setName("Todd Hockney");
+            character3.setType("Team Member");
+            character3.setHealth(25);
+            character3.setWeaponEffect(2);
+            character3.setGameCredit(10);
+
+            character4.setName("Eddie Finneran");
+            character4.setType("Civilian");
+            character4.setHealth(90);
+            character4.setWeaponEffect(0);
+            character4.setGameCredit(30);
+
+            Console.WriteLine("******* Welcome to fps Extreme *******");
+            Console.WriteLine("The Characters in this game are: ");
+            Console.WriteLine(character1.toString());
+            Console.WriteLine(character2.toString());
+            Console.WriteLine(character3.toString());           // printing the characters and attributes
+            Console.WriteLine(character4.toString());
+                                                        
+            character1.SpendAllGameCredits();
+            character4.SpendAllGameCredits();                   // applying the spend method
+
+
+        }
+    }
+}
